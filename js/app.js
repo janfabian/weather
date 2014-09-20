49e1f9baae00f3ef4cf73f8a54256e75231a6490
janfabian <jan.fabian@jetminds.com>
2014-09-20 10:05:43 +0200

    config file

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
-define(['jquery', 'underscore', 'backbone', 'router', 'views/index', 'collections/forecasts'], function($, _, Backbone, Router, IndexView, Forecasts) {
+define(['jquery',
+    'underscore',
+    'backbone',
+    'router',
+    'views/index',
+    'collections/forecasts',
+    'models/config'
+], function ($, _, Backbone, Router, IndexView, Forecasts, config) {
 
     return {
-        initialize: function() {
+        initialize: function () {
+            this.setupConfig();
+
             this.forecasts = new Forecasts();
             this.router = new Router(this);
             Backbone.history.start();
             this.indexView = new IndexView(this).render();
+        },
+        setupConfig: function () {
+            if (localStorage.getItem('tempUnits')) {
+                config.set('tempUnits', localStorage.getItem('tempUnits'));
+            }
+            if (localStorage.getItem('speedUnits')) {
+                config.set('speedUnits', localStorage.getItem('speedUnits'));
+            }
         }
     };
 
