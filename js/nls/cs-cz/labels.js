919ffccbf5a08df5b02620e17a63bcd49bd15118
janfabian <jan.fabian@jetminds.com>
2014-09-20 09:31:39 +0200

    language support

---
+++ js/nls/cs-cz/labels.js
@@ -0,0 +1,12 @@
+define({
+    days: {
+        monday: 'pondělí',
+        tuesday: 'úterý',
+        wednesday: 'středa',
+        thursday: 'čtvrtek',
+        friday: 'pátek',
+        saturday: 'sobota',
+        sunday: 'neděle'
+    },
+    enter_city_label: 'Napište název města nebo země'
+});
---
+++ js/nls/labels.js
@@ -0,0 +1,15 @@
+define({
+    root: {
+        days: {
+            monday: 'mo',
+            tuesday: 'tue',
+            wednesday: 'wed',
+            thursday: 'th',
+            friday: 'fri',
+            saturday: 'sat',
+            sunday: 'sun'
+        },
+        enter_city_label: 'Enter a city or country'
+    },
+    'cs-cz': true
+});
